f90e77ee31d4caac983d62d3279f63673effa098
Kevin Zittle <kevin@f4design.com>
2011-04-19 10:20:58 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProMag::Application.config.secret_token = 'ba141d5658ee20a934491d84699c49771243ed638a9b823eacf89c8f386bd463b269fefa4ab88a6f7878376182e4ac8f2ff6c1fd0f8578bbb67f0779d52361b7'
